d8271615c9e4f6e2d24506aaa31d2cdc355211b8
a.tsiporin <a.tsiporin@pravo.tech>
2024-07-06 23:28:34 +0500

    Реализовано логгирование в Apache Kafka.
    Исправление к коммиту c155304107e40cae8834c9e0f20f1b49f1102edb:
    
    К прежнему коммиту не была добавлена реализация структуры Notifier.

---
+++ internal/notify/kafka/notify.go
@@ -0,0 +1,54 @@
+package kafkanotify
+
+import (
+	"context"
+	"net"
+	"strconv"
+
+	"github.com/mailru/easyjson"
+	"github.com/segmentio/kafka-go"
+	"github.com/trb1maker/gobannercircle/internal/notify"
+)
+
+func NewKafkaNotify(host string, port int, topic string, partition int) *Notify {
+	return &Notify{
+		addr:      net.JoinHostPort(host, strconv.Itoa(port)),
+		topic:     topic,
+		partition: partition,
+	}
+}
+
+type Notify struct {
+	conn      *kafka.Conn
+	addr      string
+	topic     string
+	partition int
+}
+
+func (n *Notify) Connect(ctx context.Context) error {
+	var err error
+
+	n.conn, err = kafka.DialLeader(ctx, "tcp", n.addr, n.topic, n.partition)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (n *Notify) Notify(ctx context.Context, message notify.Message) error {
+	data, err := easyjson.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	if _, err := n.conn.Write(data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (n *Notify) Close() error {
+	return n.conn.Close()
+}
