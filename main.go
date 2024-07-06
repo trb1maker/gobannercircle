8c86511d64f23c3f5917e123897ce4d4b20c8c52
a.tsiporin <a.tsiporin@pravo.tech>
2024-07-07 00:43:54 +0500

    Добавил команду миграций Postgres

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/trb1maker/gobannercircle/cmd"
+import (
+	"embed"
+
+	"github.com/trb1maker/gobannercircle/cmd"
+)
+
+//go:embed migrations/postgres/*
+var migrations embed.FS
 
 func main() {
+	cmd.MigrationsPipe(migrations)
 	cmd.Execute()
 }
